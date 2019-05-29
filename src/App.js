d919d8c880f829ca5dc322ba50e65dfefa4ced91
sayantanimondal <sayantanimo@gmail.com>
2019-05-29 22:59:54 +0530

    understanding state and props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello Gonu and Katu love you babies. ummmaaaaaaa
-        </p>
-        
-      </header>
-    </div>
-  );
+
+export interface AppProps {
+    name: string;
+    teacher: string;
+    student: string;
 }
 
-export default App;
+export interface AppState {
+    name: string;
+    age: number;
+    address: string;
+}
+
+class App extends Component<AppProps, AppState> {
+
+
+    constructor(props){
+        super(props);
+
+        console.log(props.name);
+        
+        this.state = {
+            name: 'Bday',
+            age: 'sdadda',
+            address: 'whitefield'      
+        };
+
+
+    }
+
+
+
+    render() {
+        const { name } = this.props; 
+        const { age } = this.state;
+        return (
+            <React.Fragment>
+              <p>Hello Gonu and Kathu</p>
+              <p>hi</p>
+              <span>{name}</span>
+              <span>{age}</span>
+            </React.Fragment>
+
+        );
+
+    }
+
+}
+
+ export default App;
